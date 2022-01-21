3671d8cbd71be7672db0262eb6281589c4ea2b6b
Shahad Kadhim <almuhandisa.sh@gmail.com>
2022-01-21 14:42:08 +0300

    nav to home when already login

--- app/src/main/java/com/shahad/app/my_school/ui/identity/IdentityActivity.kt
+++ app/src/main/java/com/shahad/app/my_school/ui/identity/IdentityActivity.kt
@@ -1,22 +1,40 @@
 package com.shahad.app.my_school.ui.identity
 
 import android.content.Intent
+import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.findNavController
 import com.shahad.app.my_school.PreferencesKeys
 import com.shahad.app.my_school.R
+import com.shahad.app.my_school.dataStore
 import com.shahad.app.my_school.databinding.ActivityIdentityBinding
 import com.shahad.app.my_school.ui.base.BaseActivity
 import com.shahad.app.my_school.ui.main.MainActivity
 import com.shahad.app.my_school.util.writeTo
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.map
 
 @AndroidEntryPoint
 class IdentityActivity : BaseActivity<ActivityIdentityBinding>() {
 
     override fun getLayoutId() = R.layout.activity_identity
     override val viewModel: IdentityViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        checkIfAlreadyLogin()
+        super.onCreate(savedInstanceState)
+    }
+
+    private fun checkIfAlreadyLogin(){
+        lifecycleScope.launchWhenCreated {
+            this@IdentityActivity.dataStore.data
+                .collect { preferences ->
+                    preferences[PreferencesKeys.Token]?.let { navToHome() }
+                }
+        }
+    }
 
     override fun onSupportNavigateUp(): Boolean {
         findNavController(R.id.fragment_host_main).navigateUp()
