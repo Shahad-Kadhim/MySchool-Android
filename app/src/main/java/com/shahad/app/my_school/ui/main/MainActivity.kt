8e918e705617cb05a886479ed7d20489141e72e2
Shahad Kadhim <almuhandisa.sh@gmail.com>
2022-03-04 00:59:46 +0300

    fix: nav graph

--- app/src/main/java/com/shahad/app/my_school/ui/main/MainActivity.kt
+++ app/src/main/java/com/shahad/app/my_school/ui/main/MainActivity.kt
@@ -2,9 +2,11 @@ package com.shahad.app.my_school.ui.main
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.shahad.app.my_school.R
 import com.shahad.app.my_school.databinding.ActivityMainBinding
@@ -32,35 +34,44 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
                 if(it ==null){
                     navToIdentity()
                 }else{
-                    val startDestination =
-                        when(it.toRole()){
-                            Role.TEACHER -> R.id.teacher_nav
-                            Role.STUDENT -> R.id.student_nav
-                            Role.MANGER -> R.id.manger_nav
-                        }
-                    setStartDestination(startDestination)
-                    setBottomNavigation()
+                    with(it.toRole()){
+                        setStartDestination(this)
+                        setBottomNavigation(this)
+                    }
                 }
             }
         }
     }
 
-    private fun setStartDestination(startDestination: Int){
+    private fun setStartDestination(role: Role){
         findNavController(R.id.fragment_host).apply {
-            graph=navInflater.inflate(R.navigation.main_nav_graph).apply {
-                setStartDestination(startDestination)
-            }
+            graph=
+                when(role){
+                    Role.TEACHER -> navInflater.inflate(R.navigation.teacher_nav)
+                    Role.STUDENT -> navInflater.inflate(R.navigation.student_nav)
+                    Role.MANGER -> navInflater.inflate(R.navigation.manger_nav)
+                }
         }
     }
 
-    private fun setBottomNavigation() {
-        viewDataBinding.navigation
-            .setupWithNavController(findNavController(R.id.fragment_host))
+    private fun setBottomNavigation(role: Role) {
+        viewDataBinding.navigation.apply {
+            when(role){
+                    Role.TEACHER -> inflateMenu(R.menu.teacher_menu)
+                    Role.STUDENT -> inflateMenu(R.menu.student_menu)
+                    Role.MANGER -> inflateMenu(R.menu.manger_menu)
+                }
+            setupWithNavController(findNavController(R.id.fragment_host))
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
         findNavController(R.id.fragment_host).navigateUp()
         return true
+    }
+
+    override fun onBackPressed() {
+        if(!findNavController(R.id.fragment_host).navigateUp()) super.onBackPressed()
     }
 
     fun navToIdentity(){
