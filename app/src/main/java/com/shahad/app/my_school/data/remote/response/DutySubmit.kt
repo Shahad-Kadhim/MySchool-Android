08ef62717f1de4309343b094ea64f1efdfc42f07
Shahad Kadhim <almuhandisa.sh@gmail.com>
2022-04-17 02:24:34 +0300

    show name of student

--- app/src/main/java/com/shahad/app/my_school/data/remote/response/DutySubmit.kt
+++ app/src/main/java/com/shahad/app/my_school/data/remote/response/DutySubmit.kt
@@ -4,6 +4,6 @@ data class DutySubmit(
     val studentId: String,
     val dutyId: String,
     val submitDate: Long?,
-    val solutionLink: String
-
+    val solutionLink: String,
+    val studentName: String
 )
