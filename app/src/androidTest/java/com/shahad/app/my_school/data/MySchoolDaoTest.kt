9d24854e9ececd87de41154ca91a2b93e23a8c42
Shahad Kadhim <almuhandisa.sh@gmail.com>
2022-07-08 19:30:16 +0300

    testing database done.

--- app/src/androidTest/java/com/shahad/app/my_school/data/MySchoolDaoTest.kt
+++ app/src/androidTest/java/com/shahad/app/my_school/data/MySchoolDaoTest.kt
@@ -5,14 +5,14 @@ import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.SmallTest
+import com.google.common.truth.Truth.assertThat
 import com.shahad.app.my_school.data.local.MySchoolDao
 import com.shahad.app.my_school.data.local.MySchoolDatabase
 import com.shahad.app.my_school.data.local.entities.ClassEntity
 import com.shahad.app.my_school.data.local.entities.SchoolsEntity
-import junit.framework.TestCase
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.test.runBlockingTest
+import kotlinx.coroutines.runBlocking
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
@@ -20,12 +20,12 @@ import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 @SmallTest
-class MySchoolDaoTest: TestCase() {
-    lateinit var dataBase: MySchoolDatabase
-    lateinit var dao: MySchoolDao
+class MySchoolDaoTest{
+    private lateinit var dataBase: MySchoolDatabase
+    private lateinit var dao: MySchoolDao
 
     @Before
-    override fun setUp() {
+    fun setup() {
         val context = ApplicationProvider.getApplicationContext<Context>()
         dataBase = Room.inMemoryDatabaseBuilder(context, MySchoolDatabase::class.java)
             .allowMainThreadQueries()
@@ -34,42 +34,41 @@ class MySchoolDaoTest: TestCase() {
     }
 
     @After
-    override fun tearDown() {
+    fun closeDatabase() {
         dataBase.close()
     }
 
     @ExperimentalCoroutinesApi
     @Test
-    fun should_SchoolsAdded_When_InsertIt() = runBlockingTest {
+    fun should_SchoolsAdded_When_InsertIt() = runBlocking {
         //given
-        val school  = listOf(
+        val newSchools  = listOf(
             SchoolsEntity("1","School1"),
             SchoolsEntity("2","School2"),
         )
         //when
-        dao.addSchool(school)
+        dao.addSchool(newSchools)
         val schools = dao.getSchools().first()
         //then
+        assertThat(schools.containsAll(newSchools)).isTrue()
 
     }
 
 
     @ExperimentalCoroutinesApi
     @Test
-    fun should_ClassesAdded_When_InsertIt() = runBlockingTest {
+    fun should_ClassesAdded_When_InsertIt() = runBlocking {
         //given
-        val classes  = listOf(
-            ClassEntity("1","Class1","teacher",2),
-            ClassEntity("2","Class2","teacher",4),
+        val newClasses = listOf(
+            ClassEntity("1", "Class1", "teacher", 2),
+            ClassEntity("2", "Class2", "teacher", 4),
         )
         //when
-        dao.addClasses(classes)
-        val schools = dao.getCLasses().first()
+        dao.addClasses(newClasses)
+        val classes = dao.getCLasses().first()
         //then
-
-
+        assertThat(classes.containsAll(newClasses)).isTrue()
     }
-
 
 
 }
